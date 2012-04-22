636794af0c25ae6939b36f50d577dc8c34ea8e7e
Dominic Sanchez <ds8516@gmail.com>
2012-04-22 18:49:14 -0500

    Cleaner way to make data work and also seems to improve battery life greatly

--- telephony/java/com/android/internal/telephony/RIL.java
+++ telephony/java/com/android/internal/telephony/RIL.java
@@ -1411,7 +1411,7 @@ public final class RIL extends BaseCommands implements CommandsInterface {
 
         rr.mp.writeInt(command);
         rr.mp.writeInt(fileid);
-        path = null; // apparently?
+        path = null;
         rr.mp.writeString(path);
         rr.mp.writeInt(p1);
         rr.mp.writeInt(p2);
